c7b979e925874ac6aab0d15fbf0c6db63f3b7505
Ghous M. <codestargm@gmail.com>
2025-06-29 11:32:11 +0500

    change align order of heading

--- src/components/Headings.jsx
+++ src/components/Headings.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Headings({ title, desc }) {
   return (
-    <div className="w-full flex items-center justify-center flex-col py-10">
-      <h3 className="text-4xl font-extrabold   ">{title}</h3>
-      <p className="">{desc}</p>
+    <div className="w-full space-y-1 flex items-start justify-start flex-col py-10">
+      <h3 className="text-4xl font-extrabold  text-gray-700 ">{title}</h3>
+      <p className="font-medium text-gray-600">{desc}</p>
     </div>
   );
 }
