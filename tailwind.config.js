a118da54a9e789092a6787a39e327a012f115ad6
Ghous M. <codestargm@gmail.com>
2025-07-17 16:47:48 +0500

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,16 @@ module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        primary: "#0A1A2F",
-        primaryPurp:"#110A14",
-        secondary: "#F4A300",
-        secondaryCyan: "#00D1B2",
+        primary: "#2C1338",
+        secondary: "#FF4081",
         darkText: "#252525",
+        borderClr: "#CCB2BA",
       },
     },
   },
