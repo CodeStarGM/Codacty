c3379d9d336635e31c81057d516e8a8901aa5f84
Ghous M. <codestargm@gmail.com>
2025-07-24 11:51:15 +0500

    add section

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <footer className="bg-gray-50 text-gray-900 px-10 py-4">
+        <div className=" mx-auto flex flex-col md:flex-row items-center justify-between gap-6">
+          {/* Left: Vibe Line */}
+          <div className="flex items-center gap-6 text-sm ">
+            <div className="bg-red-400 rounded-full size-10" />
+            <p className="text-center md:text-left text-sm text-gray-500">
+              Fake candidates hate us. Real teams love us.
+            </p>
+          </div>
+          <div className="mt-8 text-center text-xs text-gray-500">
+            © 2025 Codacty. All rights reserved.
+          </div>
+
+          {/* Right: Links */}
+        </div>
+
+        {/* Bottom: Copyright */}
+      </footer>
+    </>
+  );
+}
