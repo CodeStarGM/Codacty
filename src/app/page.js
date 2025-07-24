9b173940ca5354b6a64cba1f48dc96b0a0f71804
Ghous M. <codestargm@gmail.com>
2025-07-24 11:50:53 +0500

    extract components

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
+import BuiltFor from "@/sections/BuiltFor";
 import Cheated from "@/sections/Cheated";
+import CTA from "@/sections/CTA";
+import FixHiring from "@/sections/FixHiring";
+import Footer from "@/sections/Footer";
 import Hero from "@/sections/Hero";
+import PriceCalculator from "@/sections/PriceCalculator";
 import Problem from "@/sections/Problem";
 import Solution from "@/sections/Solution";
+import YCHook from "@/sections/YCHook";
+import { Link } from "lucide-react";
 
 export default function Home() {
   return (
@@ -13,6 +20,20 @@ export default function Home() {
       <Cheated />
 
       <Solution />
+
+      <FixHiring />
+
+      <BuiltFor />
+
+      <YCHook />
+
+      <PriceCalculator />
+
+      <div className="p-4">
+        <CTA />
+      </div>
+
+      <Footer />
     </>
   );
 }
